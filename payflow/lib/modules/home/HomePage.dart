a65fe59a4a2df11d9f210829ace39b5e18aa5e1d
Douglas Gioielli <douglasgioielli@gmail.com>
2021-06-22 14:16:37 -0300

    Update HomePage.dart

--- payflow/lib/modules/home/HomePage.dart
+++ payflow/lib/modules/home/HomePage.dart
@@ -1,11 +1,17 @@
 
 
-class HomePage extends StatelessWidgeconst HomePage({ Key? key }) : super(key: key);
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Page"),
+      ),
     );
   }
 }
+
